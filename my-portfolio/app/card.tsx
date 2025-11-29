7d3e037d528e91f0b889a6f5a51a66ffb2b50a12
Ethan <ethancaff@gmail.com>
2025-11-29 09:56:05 +0000

    feat: added scaffold & general theme
    Updated structure of page to be more in line with end goal. Added transition svg & template cards

---
+++ my-portfolio/app/card.tsx
@@ -0,0 +1,18 @@
+export default function Card (){
+    return (
+        <div className="rounded-lg overflow-hidden shadow-xl bg-zinc-100 p-3">
+            <img className="w-full rounded-lg" src="/thumbnail-1.webp" alt="Sunset in the mountains"></img>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 text-black">The Coldest Sunset</div>
+                <p className="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+                </div>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+        </div>
+    )
+}
---
+++ my-portfolio/app/hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero (){
+    return (
+        <div className="flex flex-col items-center gap-10 px-30 pt-60 text-left w-full">
+          <h1 className="max-w-lg lg:text-9xl md:text-7xl text-5xl font-semibold text-left text-black dark:text-zinc-50">
+            Welcome
+          </h1>
+          <p className="max-w-md lg:text-xl md:text-lg sm:text-md leading-8 text-zinc-600 dark:text-zinc-200">
+            Hi! I am a fullstack software engineer working in web SAAS. Here are a list of personal projects I have completed over the years. Any queries email me at ethancaff@gmail.com
+          </p>
+        </div>
+    )
+}
