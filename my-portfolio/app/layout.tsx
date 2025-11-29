25930d90ac96d0c99a5da545be364d37bab1a94b
Ethan <ethancaff@gmail.com>
2025-11-29 23:16:46 +0000

    metadata configuration for site

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -21,7 +21,28 @@ const hostGrotesk = Host_Grotesk({
 })
 
 export const metadata: Metadata = {
-  description: "Ethan's portfolio website ",
+  title: "caffrey.dev",
+  description: "Ethan's portfolio website",
+  openGraph: {
+    title: "caffrey.dev",
+    description: "Ethan's portfolio website",
+    url: "https://caffrey.dev",
+    type: "website",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 256,
+        height: 256,
+        alt: "Portfolio Favicon",
+      }
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "caffrey.dev",
+    description: "Ethan's portfolio website",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
