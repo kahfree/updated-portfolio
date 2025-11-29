5403f348da3f88bd1cdfbcf6e67c7246c44b6de2
Ethan <ethancaff@gmail.com>
2025-11-29 23:08:07 +0000

    Update site metadata

--- my-portfolio/app/card.tsx
+++ my-portfolio/app/card.tsx
@@ -7,8 +7,8 @@ interface ICardProps {
     tags: string[]
 }
 
-export default function Card (props: ICardProps){
-    const {title, description, link, tags} = props
+export default function Card(props: ICardProps) {
+    const { title, description, link, tags } = props
     return (
         <div className="rounded-md overflow-hidden drop-shadow-xl/25 bg-zinc-100 p-3">
             {/* <img className="w-full rounded-md" src={imageSrc} alt="Sunset in the mountains"></img> */}
@@ -17,13 +17,13 @@ export default function Card (props: ICardProps){
                 <p className="text-gray-700 text-base">
                     {description}
                 </p>
-                </div>
+            </div>
             <div className="px-6 pt-4 pb-2">
                 {tags.map(tag => <span className="inline-block bg-pink-900 rounded-full px-3 py-1 text-sm font-semibold text-slate-50 mr-2 mb-2 glow">#{tag}</span>)}
             </div>
-            <hr/>
+            <hr />
             <div className="p-6">
-                <a href={link} target="_blank"><DiGithubBadge className="fill-black scale-200"/></a>
+                <a href={link} target="_blank"><DiGithubBadge className="fill-black scale-200" /></a>
             </div>
         </div>
     )
